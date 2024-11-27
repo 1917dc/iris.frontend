201351273af946e89aa4f6b14dbfefcbb40d761f
Luiz Gustavo <142899763+1917dc@users.noreply.github.com>
2024-11-27 11:24:05 -0300

    Arrumando validação de token

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,23 +1,40 @@
+import { BACKEND_URL } from "$env/static/private";
 import {type Handle, redirect} from "@sveltejs/kit";
 import { jwtDecode } from "jwt-decode";
 
 export const handle = (async ({ event, resolve }) => {
-    const tokenCookie = event.cookies.get("token");
-    let token : string | null = null;
+    const { cookies } = event
+    const cookieToken = cookies.get("token")
+    let token : string | null = null
 
-    try{
-        if(typeof tokenCookie === "string"){
-            token = tokenCookie;
+    try {
+        if(typeof cookieToken === "string"){
+            token = cookieToken
+
+            const response = await fetch(BACKEND_URL + 'auth/validate/token', {
+                method: 'POST',
+                body: JSON.stringify({
+                    "token" : token
+                })
+            })
+
+            if(!response.ok){
+                throw redirect(308, '/')
+            }
         }
-    } catch(e) {
-        console.error(`Error parsing token cookie: ${e}`);
+
+        
+    } catch(e : any){
+        console.error(`Erro ao assimilar token: ${e.message}`)
     }
+
+    // TODO: Fazer o tratamento de exceção do token de usuário
+    //       e fazer uma tela personalizada de erro, que dá a opção de efetuar o login de novo. 
 
     if(!token){
         return resolve(event)
     }
 
     event.locals.token = token;
-
     return resolve(event);
 }) satisfies Handle;
--- src/routes/auth/logout/+server.ts
+++ src/routes/auth/logout/+server.ts
@@ -2,10 +2,10 @@ import { redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ cookies }) => {
-    cookies.set('user', '', {
+    cookies.set('token', '', {
         path: '/',
         expires: new Date(0)
     })
 
-    return redirect(307, '/');
+    return redirect(308, '/');
 };
