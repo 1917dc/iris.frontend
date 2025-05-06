b4341a97eeb5e6cc1259c2b590693f41525dcbfd
kr°l <101878278+krol-rustns@users.noreply.github.com>
2025-05-06 19:38:42 -0300

    Registro de turma finalizado

--- src/routes/protected/coordenador/registrar/disciplinas/+page.server.ts
+++ src/routes/protected/coordenador/registrar/disciplinas/+page.server.ts
@@ -5,27 +5,74 @@ import type { Professor } from '$lib/types/Professor';
 import { zod } from 'sveltekit-superforms/adapters'
 import { superValidate } from 'sveltekit-superforms'
 import { handleError } from '$lib/components/notificator';
-import type { Actions } from '@sveltejs/kit';
+import { type Actions, fail } from '@sveltejs/kit';
+import { setFlash } from 'sveltekit-flash-message/server';
+
+const registerSchema = z.object({
+    identificador: z.string().min(1, { message: "Campo obrigatório." }),
+    sala: z.string().min(1, { message: "Campo obrigatório." }),
+    disciplina: z.string().min(1, { message: "Campo obrigatório." }),
+    professor: z.string().min(1, { message: "Selecione um professor." }),
+});
 
 export const load = (async ({cookies, locals}) => {
+    const form = await superValidate(zod(registerSchema));
+
     const response = await fetch(`${BACKEND_URL}/coordenador/professores`, {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
             "Authorization": `Bearer ${locals.token}`
         }
-    })
+    });
     
     if(!response.ok){
         console.error(response.status);
         return await handleError(response, cookies);
     }
     const professores: Professor[] = await response.json();
-    return { professores };
+    return { form, professores };
 }) satisfies PageServerLoad;
 
 export const actions: Actions = {
     post: async({ request, cookies, locals }) => {
+        const form = await superValidate(request, zod(registerSchema));
+
+        if (!form.valid) {
+            return fail(400, { form });
+        }
+
+        const { identificador, sala, disciplina, professor } = form.data;
+
+        const payload = {
+            identificador,
+            sala,
+            disciplina,
+            professorCPF: professor
+        };
+
+        console.log("Payload enviado:", payload);
+
+
+        const response = await fetch(`${BACKEND_URL}/coordenador/cadastrar-turma`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${locals.token}`
+            },
+            body: JSON.stringify(payload)
+        });
+
+
+        console.log("Resposta do back-end:", await response.json());
+
+        if (!response.ok) {
+            return await handleError(response, cookies);
+        }
+
+        setFlash({ type: 'success', message: 'Turma registrada com sucesso!' }, cookies);
+
+        return { form };
         
     }
 }
